b73040ff4fc8788d6be339b0d78bfdec7d142c80
fatimatuzzahra002 <fatimatuzzahra035@gmail.com>
2021-09-30 10:51:03 +0800

    ini push terbaru

---
+++ app/src/main/java/com/example/smartskripsi/ListDay.java
@@ -0,0 +1,14 @@
+package com.example.smartskripsi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListDay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_day);
+    }
+}
